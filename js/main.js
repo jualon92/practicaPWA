4e506c687054e977038ecf59c4e1981b63f55201
jualon92 <londrajuan@gmail.com>
2022-03-28 02:00:49 -0300

    indice relativo

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ function agregarListeners(){
 function registrarServiceWorker(){
     //verificar si nav es compatible con sv
     if ("serviceWorker" in navigator){
-        navigator.serviceWorker.register("/PRACTICA-PWA/sw.js")
+        navigator.serviceWorker.register("sw.js")
         .then( reg => {
             console.log("El service worker se registro correctamente", reg)
         })
